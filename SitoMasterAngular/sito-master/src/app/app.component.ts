608009892a30bea5b7ff36fb09f6608df4daf86f
Lore04721 <agolinolory@gmail.com>
2025-10-27 13:19:50 +0100

    Aggiornamento sito master

--- SitoMasterAngular/sito-master/src/app/app.component.ts
+++ SitoMasterAngular/sito-master/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'sito-master';
-}
+export class AppComponent {}
