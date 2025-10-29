aecfeddd8a9ce9f53fb9e8e58f1bf5f9b3aeb182
Zephir18 <gabrielemaugeri03@gmail.com>
2025-10-29 17:22:55 +0100

    prova

---
+++ SitoMasterV2/Sito-Master/src/app/features/auth/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginComponent {}
